4a3db57c084f76954c9e3becec18cd14b5804b95
Heng-Hao Pu <hpu@oswego.edu>
2018-04-26 22:47:42 -0400

    Added interaction: link search bar to houses

---
+++ scripts.js
@@ -0,0 +1,11 @@
+spotX = function(searchBarId) {
+    var searchInputText = document.getElementById(searchBarId).value
+    if (searchInputText.toLowerCase() === "lawyer") {
+        //console.log(document.getElementById("square-4-7"))
+        //document.getElementById("square-4-7").style.background = "grey"
+        document.getElementById("square-4-7").children[0].children[0].src = "/images/houses-blue.svg"
+        document.getElementById("square-4-7").children[0].children[0].style.width = '100%';   
+    } else {
+        document.getElementById("square-4-7").children[0].children[0].src = "/images/houses.svg"
+    }
+} 
