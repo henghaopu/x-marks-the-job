a2c2a802d52b186abfd47713cca62774a2cf7b06
Heng-Hao Pu <hpu@oswego.edu>
2018-04-28 01:14:30 -0400

    Refine the interaction between the main page and the job info page

--- scripts.js
+++ scripts.js
@@ -1,11 +1,78 @@
+window.onload = function() {
+    var searchBar = document.getElementById("search-bar");
+    if (searchBar !== null) {
+        if (searchBar.value === "lawyer") {
+            markLawyerJobPosition()    
+        } else {
+            clearLawyerJobPosition()
+        }
+    }
+}
+
 spotX = function(searchBarId) {
     var searchInputText = document.getElementById(searchBarId).value
     if (searchInputText.toLowerCase() === "lawyer") {
-        //console.log(document.getElementById("square-4-7"))
-        //document.getElementById("square-4-7").style.background = "grey"
-        document.getElementById("square-4-7").children[0].children[0].src = "/images/houses-blue.svg"
-        document.getElementById("square-4-7").children[0].children[0].style.width = '100%';   
+        markLawyerJobPosition()
     } else {
-        document.getElementById("square-4-7").children[0].children[0].src = "/images/3d-house.svg"
+        clearLawyerJobPosition()
     }
 } 
+
+spotKeyword = function(keywordId) {
+    var id = keywordId.toLowerCase()
+    var jobTitle = document.getElementById(id).innerHTML
+    switch (jobTitle) {
+        case "lawyer":
+            var searchBar = document.getElementById("search-bar")
+            searchBar.value = "lawyer"
+            searchBar.onchange()
+            break
+        case "baker":
+            break;
+        case "carpenter":
+            break;
+    }
+}
+
+//goToJobs = function(imageId) {
+//    var id = imageId.toLowerCase()
+//    var anchor = document.getElementById(id).parentNode.click()
+//    var imageSource = document.getElementById(id).src
+//    var imageSourceArray = imageSource.split('/')
+//    var imageName = imageSourceArray[imageSourceArray.length - 1]
+//    console.log(imageName)
+//    if(imageName === "3d-building-5-ffcc00.svg") {
+//        window.location.href = "job.html";
+//    }
+//}
+
+function markLawyerJobPosition() {
+    var square1 = document.getElementById("square-5-6")
+    var square2 = document.getElementById("square-2-5")
+    var square3 = document.getElementById("square-2-6")
+    // building links
+    square1.children[0].setAttribute("href", "job.html")
+    square2.children[0].setAttribute("href", "job.html")
+    square3.children[0].setAttribute("href", "job.html")
+    // building images
+    square1.children[0].children[0].src = "/images/3d-building-5-ffcc00.svg"
+    square2.children[0].children[0].src = "/images/3d-building-4-ffcc00.svg"
+    square3.children[0].children[0].src = "/images/3d-building-4-ffcc00.svg"
+    square1.children[0].children[0].style.width = '100%' 
+    square2.children[0].children[0].style.width = '100%' 
+    square3.children[0].children[0].style.width = '100%'
+}
+
+function clearLawyerJobPosition() {
+    var square1 = document.getElementById("square-5-6")
+    var square2 = document.getElementById("square-2-5")
+    var square3 = document.getElementById("square-2-6")
+    // building links
+    square1.children[0].setAttribute("href", "javascript:void(0)")
+    square2.children[0].setAttribute("href", "javascript:void(0)")
+    square3.children[0].setAttribute("href", "javascript:void(0)")
+    // building images
+    square1.children[0].children[0].src = "/images/3d-building-5.svg"
+    square2.children[0].children[0].src = "/images/3d-building-4.svg"
+    square3.children[0].children[0].src = "/images/3d-building-4.svg"
+}
