fc4efa5de5fad6f5770a07b90a1484ef35ba0d37
Heng-Hao Pu <hpu@oswego.edu>
2018-04-27 20:21:31 -0400

    Added terrain and 3d buildings

--- scripts.js
+++ scripts.js
@@ -6,6 +6,6 @@ spotX = function(searchBarId) {
         document.getElementById("square-4-7").children[0].children[0].src = "/images/houses-blue.svg"
         document.getElementById("square-4-7").children[0].children[0].style.width = '100%';   
     } else {
-        document.getElementById("square-4-7").children[0].children[0].src = "/images/houses.svg"
+        document.getElementById("square-4-7").children[0].children[0].src = "/images/3d-house.svg"
     }
 } 
